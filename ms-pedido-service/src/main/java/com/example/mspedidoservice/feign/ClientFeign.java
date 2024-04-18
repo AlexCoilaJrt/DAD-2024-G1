484b5bc8c42172b7acb95283e83576b4bdbeb74b
AlexCoilaJrt <104539425+AlexCoilaJrt@users.noreply.github.com>
2024-04-17 22:37:17 -0500

    act

--- ms-pedido-service/src/main/java/com/example/mspedidoservice/feign/ClientFeign.java
+++ ms-pedido-service/src/main/java/com/example/mspedidoservice/feign/ClientFeign.java
@@ -6,7 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-catalogo-service",path = "/categoria")
+@FeignClient(name = "ms-client-service",path = "/categoria")
 public interface ClientFeign {
     @GetMapping("/{id}")
     public ResponseEntity<Client> listById(@PathVariable(required = true) Integer id);
