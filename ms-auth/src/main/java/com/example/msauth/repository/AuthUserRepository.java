6c67b1b139d6a13221c4ea18c8094cbeefb7494c
AlexCoilaJrt <104539425+AlexCoilaJrt@users.noreply.github.com>
2024-05-16 09:53:32 -0500

    al

--- ms-auth/src/main/java/com/example/msauth/repository/AuthUserRepository.java
+++ ms-auth/src/main/java/com/example/msauth/repository/AuthUserRepository.java
@@ -8,5 +8,5 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 @Repository
 public interface AuthUserRepository extends JpaRepository<AuthUser, Integer> {
-    Optional<AuthUser> findByUsername(String username);
+    Optional<AuthUser> findByUsername(String userName);
 }
